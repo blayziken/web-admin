60bbf144ef0d252f67cad2d28afadea25109f166
blayziken <kingtoluwalope@gmail.com>
2021-09-04 11:58:32 +0100

    layout AppBar done

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web_crash_course/helpers/responsiveness.dart';
 import 'package:flutter_web_crash_course/widgets/large_screen.dart';
 import 'package:flutter_web_crash_course/widgets/small_screen.dart';
+import 'package:flutter_web_crash_course/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
+      key: scaffoldKey,
+      appBar: topNavigationBar(context, scaffoldKey),
+      drawer: Drawer(),
       body: ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
---
+++ lib/widgets/top_nav.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_crash_course/constants/style.dart';
+import 'package:flutter_web_crash_course/helpers/responsiveness.dart';
+import 'package:flutter_web_crash_course/widgets/custom_text.dart';
+
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                key.currentState!.openDrawer();
+              },
+            ),
+      elevation: 0,
+      title: Row(
+        children: [
+          Visibility(
+            child: CustomText(
+              text: "Dash",
+              color: lightGrey,
+              size: 20,
+              weight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: dark.withOpacity(.7),
+            ),
+            onPressed: () {},
+          ),
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(Icons.notifications, color: dark.withOpacity(.7)),
+                onPressed: () {},
+              ),
+              Positioned(
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: active,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: light, width: 2),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 22,
+            width: 1,
+            color: lightGrey,
+          ),
+          SizedBox(width: 24),
+          CustomText(
+            text: "Santos Enoque",
+            color: lightGrey,
+            size: null,
+            weight: null,
+          ),
+          SizedBox(width: 16),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Container(
+              padding: EdgeInsets.all(2),
+              margin: EdgeInsets.all(2),
+              child: CircleAvatar(
+                backgroundColor: light,
+                child: Icon(
+                  Icons.person_outline,
+                  color: dark,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      iconTheme: IconThemeData(color: dark),
+      backgroundColor: Colors.transparent,
+    );
